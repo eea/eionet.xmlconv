362ebf393d003c0b2d602c7b00fa89a37d21bcb5
te-ee <te-ee@localhost>
2004-02-17 14:30:44 +0000

    XQueryTask - usage of saxon api has changed

--- src/eionet/gdem/qa/XQueryTask.java
+++ src/eionet/gdem/qa/XQueryTask.java
@@ -5,7 +5,41 @@ import eionet.gdem.Utils;
 import java.sql.SQLException;
 import java.io.IOException;
 import java.io.File;
+import java.io.Reader;
+import java.io.FileReader;
+import java.io.FileOutputStream;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.Properties;
+import java.util.List;
+import java.util.Iterator;
 
+import net.sf.saxon.Configuration;
+import net.sf.saxon.query.StaticQueryContext;
+import net.sf.saxon.query.DynamicQueryContext;
+import net.sf.saxon.query.QueryProcessor;
+import net.sf.saxon.query.XQueryExpression;
+import net.sf.saxon.query.QueryResult;
+import net.sf.saxon.om.Item;
+import net.sf.saxon.om.NamePool;
+import net.sf.saxon.om.DocumentInfo;
+import net.sf.saxon.om.SequenceIterator;
+import net.sf.saxon.om.NodeInfo;
+import net.sf.saxon.value.Type;
+import net.sf.saxon.xpath.XPathException;
+
+import org.xml.sax.InputSource;
+import javax.xml.transform.Source;
+import net.sf.saxon.instruct.TerminationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.URIResolver;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.OutputKeys;
 
 /**
 * A task executing the XQuery task and storing the result of processing
@@ -69,16 +103,18 @@ _l("** job " + _jobId + " processing started");
 
     //CHANGE ME TO USE QUERYPROCESSOR
     String xqParam=Utils.XQ_SOURCE_PARAM_NAME + "=" + srcFile;
-    String[] args = {"-o", _resultFile, _scriptFile, xqParam};
+    //String[] args = {"-o", _resultFile, _scriptFile, xqParam};
  
       try {
 _l("** query starts: " + _jobId + " params: " + _resultFile + " " + xqParam);
 
 //FIX ME using main() is not correct and does not handle errors!!
-       net.sf.saxon.Query.main(args);
+       //net.sf.saxon.Query.main(args);
+
+        runQuery(srcFile, _scriptFile, _resultFile);
         
       } catch (Exception e ) {
-        handleError("Error processing XQ:" + e.toString())      ;
+        handleError("Error processing XQ:" + e.toString());
         return;
       }
 
@@ -127,6 +163,7 @@ private static void _l(String s ){
   * saves the error message as the result of the job?
   */
   private void handleError(String error) {
+
     try {
       _db.changeJobStatus(_jobId, Utils.XQ_ERROR);
 
@@ -136,6 +173,7 @@ private static void _l(String s ){
         //to DB as well?!?
         
       }
+      System.out.println(_resultFile);
         
       Utils.saveStrToFile(_resultFile, "<error>" + error + "</error>", "xml");
 
@@ -151,5 +189,108 @@ private static void _l(String s ){
 
   //possible clear temporary files
   private void cleanup() {
+  }
+
+  /**
+  * excetues
+  */
+
+  private void runQuery(String in, String script, String out) {
+
+    boolean wrap=false;
+    Source sourceInput = null;
+    StringBuffer err_buf = new StringBuffer();
+    try{
+
+      Configuration config = new Configuration();
+      config.setHostLanguage(config.XQUERY);
+      StaticQueryContext staticEnv = new StaticQueryContext();
+      staticEnv.setConfiguration(config);
+      DynamicQueryContext dynamicEnv = new DynamicQueryContext();
+      Properties outputProps = new Properties();
+      outputProps.setProperty(OutputKeys.INDENT, "yes");
+
+    //query script
+        Reader queryReader = new FileReader(script);
+        //FileInputStream fin = new FileInputStream(script);
+        //BufferedReader queryReader = new BufferedReader(new InputStreamReader(fin));
+        staticEnv.setBaseURI(new File(script).toURI().toString());
+    //source file
+        File sourceFile = new File(in);
+        InputSource eis = new InputSource(sourceFile.toURI().toString());
+        sourceInput = new SAXSource(eis);
+
+    //result file
+        OutputStream destination = new FileOutputStream(out);
+
+        QueryProcessor xquery = new QueryProcessor(config, staticEnv);
+    //compile Query
+        XQueryExpression exp;
+        try {
+          exp = xquery.compileQuery(queryReader);
+        } 
+        catch (XPathException err) {
+          int line = -1;
+          if (err.getLocator() != null) {
+            line = err.getLocator().getLineNumber();
+          }
+          if (line == -1) {
+            err_buf.append("Failed to compile query: ");
+          } else {
+            err_buf.append("Syntax error at line " + line + ":");
+          }
+          throw new TransformerException(err);
+        }
+
+        if (sourceInput != null) {
+          DocumentInfo doc = xquery.buildDocument(sourceInput);
+          dynamicEnv.setContextNode(doc);
+        }
+        try {
+          // The next line actually executes the query
+          //List querylist = exp.evaluate(dynamicEnv);
+          // now get a java.util iterator
+          //System.out.println("Number Nodes =  " +  querylist.size());        
+          SequenceIterator results = exp.iterator(dynamicEnv);
+
+          if (wrap) {
+            DocumentInfo resultDoc = QueryResult.wrap(results, NamePool.getDefaultNamePool());
+            QueryResult.serialize(resultDoc,
+                 new StreamResult(destination),
+                      outputProps);
+            destination.close();
+          } else {
+            PrintWriter writer = new PrintWriter(destination);
+            while (results.hasNext()) {
+              Item item = results.next();
+              switch (item.getItemType()) {
+                case Type.DOCUMENT:
+                case Type.ELEMENT:
+                  QueryResult.serialize((NodeInfo)item,
+                    new StreamResult(writer), outputProps);
+                  writer.println("");
+                  break;
+                default:
+                  writer.println(item.getStringValue());
+              }
+            }
+            writer.close();
+          }
+        }
+        catch (TerminationException err) {
+          throw err;
+        } catch (TransformerException err) {
+         // The message will already have been displayed; don't do it twice
+          throw new TransformerException("Run-time errors were reported");
+        }
+        catch (Exception err) {
+          err.printStackTrace();
+          throw err;
+        }
+                  
+    } catch (Exception e ) {
+       err_buf.append("Query processing failed: " + e.toString());
+       handleError(err_buf.toString());
+   }
   }
 }
