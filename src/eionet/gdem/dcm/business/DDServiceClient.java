a2df58e18cb88f7080db65c8e296235335db7260
Nedeljko Pavlovic <nedjo@eurodyn.com>
2005-11-01 13:34:22 +0000

    cleaning up

--- src/eionet/gdem/dcm/business/DDServiceClient.java
+++ src/eionet/gdem/dcm/business/DDServiceClient.java
@@ -1,3 +1,24 @@
+/*
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * 
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ * 
+ * The Original Code is Web Dashboards Service
+ * 
+ * The Initial Owner of the Original Code is European Environment
+ * Agency (EEA).  Portions created by European Dynamics (ED) company are
+ * Copyright (C) by European Environment Agency.  All Rights Reserved.
+ * 
+ * Contributors(s):
+ *    Original code: Istvan Alfeldi (ED)
+ */
+
 package eionet.gdem.dcm.business;
 
 import java.util.List;
@@ -10,76 +31,68 @@ import com.tee.uit.client.ServiceClients;
 
 import eionet.gdem.Properties;
 
-
-public  class DDServiceClient {
+public class DDServiceClient {
 
 	protected String serviceName = null;
-	protected String serviceUrl  = null;
-	//protected String serviceUsr  = null;
-	//protected String servicePsw  = null;
+	protected String serviceUrl = null;
 	protected ServiceClientIF client = null;
-	
+
+
 	protected void load() throws Exception {
-		
-		//if (Util.voidStr(serviceName) || Util.voidStr(serviceUrl))
-		if(serviceName==null || serviceName.equals("") || serviceUrl ==null || serviceUrl.equals(""))
-			throw new Exception("serviceName or serviceUrl is missing!");
-		
+		if (serviceName == null || serviceName.equals("") || serviceUrl == null || serviceUrl.equals("")) throw new Exception("serviceName or serviceUrl is missing!");
 		client = ServiceClients.getServiceClient(serviceName, serviceUrl);
-		//if (!Util.voidStr(serviceUsr) && !Util.voidStr(serviceUsr))
-		//	client.setCredentials(serviceUsr,servicePsw);
 	}
-	
-	protected void getProps(){
-		
-		 serviceName = Properties.invServName;
-		 serviceUrl = Properties.invServUrl;
+
+
+	protected void getProps() {
+		serviceName = Properties.invServName;
+		serviceUrl = Properties.invServUrl;
 	}
-	
-	protected Object execute(String method, Vector params) throws Exception{
-		
+
+
+	protected Object execute(String method, Vector params) throws Exception {
 		if (client == null) load();
 		return client.getValue(method, params);
 	}
-	
-	public  void execute(HttpServletRequest req) throws Exception {
-		
+
+
+	public void execute(HttpServletRequest req) throws Exception {
 	}
-	 public static void main( String args[] ) {
-		 DDServiceClient d = new DDServiceClient();
-		 try{
-			 Vector b = new Vector();
-			 d.getProps();
-			 d.load();
-			 Object res = d.execute("getDSTables", b);
-	        List list = (List) res;
-	        for(int i=0; i<list.size(); i++)
-	        {
-	          Object o = list.get(i);
-	          System.out.println( i + " - " + o );
-	        }
-			 
-		 }catch(Exception e){e.printStackTrace();}
-		 
-		 
-	 }
-	 public static List getDDTables() {
-		 DDServiceClient d = new DDServiceClient();
-		 List list = null;
-		 try{
-			 Vector b = new Vector();
-			 d.getProps();
-			 d.load();
-			 Object res = d.execute("getDSTables", b);
-	        list = (List) res;
-/*	        for(int i=0; i<list.size(); i++)
-	        {
-	          Object o = list.get(i);
-	          System.out.println( i + " - " + o );
-	        }
-	*/		 
-		 }catch(Exception e){e.printStackTrace();}
-		 return list;
-		 
-	 }
+
+
+	public static List getDDTables() {
+		DDServiceClient d = new DDServiceClient();
+		List list = null;
+		try {
+			Vector b = new Vector();
+			d.getProps();
+			d.load();
+			Object res = d.execute("getDSTables", b);
+			list = (List) res;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+	
+	
+	public static void main(String args[]) {
+		DDServiceClient d = new DDServiceClient();
+		try {
+			Vector b = new Vector();
+			d.getProps();
+			d.load();
+			Object res = d.execute("getDSTables", b);
+			List list = (List) res;
+			for (int i = 0; i < list.size(); i++) {
+				Object o = list.get(i);
+				System.out.println(i + " - " + o);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
 }
