19f585ffbc923c4f22af67e65254e83984f922c2
Nedeljko Pavlovic <nedjo@eurodyn.com>
2005-11-01 07:09:07 +0000

    cleaning up

--- src/eionet/gdem/web/struts/login/LoginAction.java
+++ src/eionet/gdem/web/struts/login/LoginAction.java
@@ -16,31 +16,24 @@
  * Copyright (C) by European Environment Agency.  All Rights Reserved.
  * 
  * Contributors(s):
- *    Original code: Dusan Popovic (ED)
- *                          Nedeljko Pavlovic (ED) 
+ *    Original code: Nedeljko Pavlovic (ED) 
  */
 
 package eionet.gdem.web.struts.login;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.apache.struts.action.Action;
-import org.apache.struts.action.ActionError;
 import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 import org.apache.struts.action.ActionMessage;
 import org.apache.struts.action.ActionMessages;
+import org.apache.struts.validator.DynaValidatorForm;
 
 import com.tee.uit.security.AppUser;
-
 
 import eionet.gdem.conversion.ssr.Names;
 import eionet.gdem.services.GDEMServices;
@@ -49,73 +42,56 @@ import eionet.gdem.utils.SecurityUtil;
 import eionet.gdem.utils.Utils;
 
 public class LoginAction extends Action {
-    
-	 private static LoggerIF _logger=GDEMServices.getLogger();
-	 protected final String GDEM_SSAclName = "/stylesheets";
-	
-    public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        ActionMessages loginMessages = new ActionMessages();
-        ActionErrors errors = new ActionErrors();
-        ActionForward ret = null;
 
-        LoginForm loginForm = (LoginForm) actionForm;
-        String username = loginForm.getUsername();
-        String password = loginForm.getPassword();
-        boolean haveCookie = false;
+	private static LoggerIF _logger = GDEMServices.getLogger();
+	protected final String GDEM_SSAclName = "/stylesheets";
 
-        //UserFacade facade = new UserFacade();
-        //User user = facade.authenticate(username, password);
-		try{
-			doLogin(username, password,httpServletRequest);
+
+	public ActionForward execute(ActionMapping actionMapping, ActionForm actionForm, HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+		ActionMessages loginMessages = new ActionMessages();
+		ActionErrors errors = new ActionErrors();
+		ActionForward ret = null;
+		DynaValidatorForm loginForm=(DynaValidatorForm) actionForm;
+
+		String username = (String) loginForm.get("username");
+		String password = (String) loginForm.get("password");
+		
+		try {
+			doLogin(username, password, httpServletRequest);
 			_logger.debug("Success login");
 			ret = actionMapping.findForward("home");
 			httpServletRequest.getSession().setAttribute("user", username);
-		}catch(Exception e){
-			_logger.debug("Fail login");
-            errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionError("label.login.error.invalid"));
-            ret = actionMapping.getInputForward();	        		
+			loginForm.getMap().clear();
+		} catch (Exception e) {
+			loginForm.set("password", null);
+			_logger.error("Fail login");
+			errors.add(ActionMessages.GLOBAL_MESSAGE, new ActionMessage("label.login.error.invalid"));
+			ret = actionMapping.getInputForward();
 		}
 		
 		httpServletRequest.getSession().setAttribute("dcm.messages", loginMessages);
-        httpServletRequest.getSession().setAttribute("dcm.errors", errors);
-		
-        return ret;
+		httpServletRequest.getSession().setAttribute("dcm.errors", errors);
+
+		return ret;
+
+	}
 
 
-    }
 
-    private void SaveCookie(HttpServletResponse httpServletResponse, String username, String password) {
-        Cookie cookie = new Cookie("user", username + " " + password);
-        cookie.setMaxAge(30 * 24 * 60 * 60); // 30 days
-        //cookie.setVersion(1);
-		//cookie.setPath("");
-        httpServletResponse.addCookie(cookie);
-    }
-    private void doLogin(String username, String password, HttpServletRequest httpServletRequest) throws Exception {
-
-
-	      try {
-	        //DirectoryService.sessionLogin(u, p);
-	        AppUser aclUser = new AppUser();
-
-	        if(!Utils.isNullStr(username))
-	          aclUser.authenticate(username,password);
-
-	        if (!SecurityUtil.hasPerm(username,GDEM_SSAclName, "v"))//GDEM_readPermission))
-	          throw new Exception("Not allowed to use the Styelsheet Repository");
-
-			
-	        //session.setAttribute(Names.USER_ATT, aclUser);
+	private void doLogin(String username, String password, HttpServletRequest httpServletRequest) throws Exception {
+		try {
+			AppUser aclUser = new AppUser();
+			if (!Utils.isNullStr(username)) aclUser.authenticate(username, password);
+			if (!SecurityUtil.hasPerm(username, GDEM_SSAclName, "v")) //GDEM_readPermission))
+				throw new Exception("Not allowed to use the Styelsheet Repository");
+			//session.setAttribute(Names.USER_ATT, aclUser);
 			//add object into session becouse of old bussines ligic
 			httpServletRequest.getSession().setAttribute(Names.USER_ATT, aclUser);
+		} catch (Exception dire) {
+			_logger.debug("Authentication failed " + dire.toString());
+			throw new Exception("Authentication failed ");
+		}
 
-	         
-	    } catch (Exception dire ){
-		  _logger.debug( "Authentication failed " + dire.toString());
-	      throw new Exception("Authentication failed ");
-	    }
-	    
-	 }
+	}
 
 }
-
