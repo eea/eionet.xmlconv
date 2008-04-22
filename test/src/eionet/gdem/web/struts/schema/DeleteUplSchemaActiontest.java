7c4a9930af88d3ee03fa9df57adbb8f4c8b5e48f
Enriko Käsper <enriko@tripledev.ee>
2008-04-22 14:38:41 +0000

    Ticket #1349: JUnit tests for UPL Schemas. StrutsTestCase library added for testing Struts actions.

---
+++ test/src/eionet/gdem/web/struts/schema/DeleteUplSchemaActiontest.java
@@ -0,0 +1,105 @@
+/*
+ * Created on 22.04.2008
+ */
+package eionet.gdem.web.struts.schema;
+
+import java.io.File;
+import java.util.Hashtable;
+
+import javax.servlet.http.HttpSession;
+
+import servletunit.struts.MockStrutsTestCase;
+import eionet.gdem.dcm.BusinessConstants;
+import eionet.gdem.services.GDEMServices;
+import eionet.gdem.services.db.dao.IUPLSchemaDao;
+import eionet.gdem.test.DbHelper;
+import eionet.gdem.test.TestConstants;
+import eionet.gdem.test.TestUtils;
+
+/**
+ * @author Enriko Käsper, TietoEnator Estonia AS
+ * DeleteUplSchemaActiontest
+ */
+
+public class DeleteUplSchemaActiontest   extends MockStrutsTestCase {
+
+	private  IUPLSchemaDao uplSchemaDao = GDEMServices.getDaoService().getUPLSchemaDao();
+	private String idSchema="10";
+
+	public DeleteUplSchemaActiontest(String testName) {
+		super(testName);
+	}
+
+	public void setUp() throws Exception {
+		super.setUp();
+		//set struts-confg file location
+		setConfigFile(TestUtils.getStrutsConfigLocation());
+		// set tempdir property for executing multi-part requests. Struts tries to save the sent file temprarily
+		context.setAttribute("javax.servlet.context.tempdir", new File(TestUtils.getStrutsTempDir(this)));
+		setInitParameter("validating","false");
+		//setup database
+		DbHelper.setUpDatabase(this, TestConstants.SEED_DATASET_UPL_SCHEMAS_XML);
+		TestUtils.setUpProperties(this);
+	}
+	/**
+	 * Tests successful deleting. Verifies the action message and forward and DB
+	 * @throws Exception 
+	 * 
+	 */
+	public void testSuccessfulForward() throws Exception {
+
+    	int countUplSchema = uplSchemaDao.getUplSchema().size();
+
+		setRequestPathInfo("/deleteUplSchema");
+
+		HttpSession session = request.getSession();
+		session.setAttribute("user", TestConstants.TEST_ADMIN_USER);
+
+		addRequestParameter("schemaId",idSchema);
+
+		actionPerform();
+		verifyForward("success");
+		verifyForwardPath("/do/uplSchemas");
+		//verifyTilesForward("success", "/do/uplSchemas");
+		verifyNoActionErrors();
+		String[] actionMess = {"label.uplSchema.deleted"};
+		verifyActionMessages(actionMess);
+
+
+        //check if the row was deleted or not
+        int countUplSchema2 = uplSchemaDao.getUplSchema().size();
+        assertEquals(countUplSchema-1,countUplSchema2);
+	}
+	/**
+	 * Editing failed, because of lack of permissins
+	 * @throws Exception 
+	 * 
+	 */
+	public void testFailedNotPermissions() throws Exception {
+
+    	int countUplSchema = uplSchemaDao.getUplSchema().size();
+
+		setRequestPathInfo("/deleteUplSchema");
+
+		HttpSession session = request.getSession();
+		session.setAttribute("user", TestConstants.TEST_USER);
+
+		addRequestParameter("schemaId",idSchema);
+
+		actionPerform();
+		verifyForward("success");
+		verifyForwardPath("/do/uplSchemas");
+		String[] errMess = {BusinessConstants.EXCEPTION_AUTORIZATION_SCHEMA_DELETE};
+		verifyActionErrors(errMess);
+
+		//Get schema by ID and test if it still exists in DB
+		Hashtable schema = uplSchemaDao.getUplSchemaById(idSchema);	
+		assertEquals((String)schema.get("schema_id"),idSchema);
+
+		//check if the row was deleted or not
+        int countUplSchema2 = uplSchemaDao.getUplSchema().size();
+        assertEquals(countUplSchema,countUplSchema2);
+
+	}
+}
+
