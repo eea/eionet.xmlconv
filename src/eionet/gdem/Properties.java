ac6ef2525fb3824c594dd66593584c71c5049197
Enriko Käsper <enriko@tripledev.ee>
2011-05-25 08:30:24 +0000

    TechnicalDebt: checkstyle fixes

--- src/eionet/gdem/Properties.java
+++ src/eionet/gdem/Properties.java
@@ -30,144 +30,158 @@ import java.util.ResourceBundle;
 import org.apache.log4j.Logger;
 
 /**
- * Several settings and properties for GDEM
+ * Several settings and properties for XMLCONV application.
  */
 public class Properties {
 
+    /** Application classes (WEB-INF/classes) path in file system. */
     public static String appHome = null;
-
+    /** Folder for temporary files. */
     public static String tmpFolder = "/tmp";
-
+    /** Folder for XSLs. */
     public static String xslFolder = "/xsl/";
-
+    /** Folder for OpenDocument helper files. */
     public static String odsFolder = "/opendoc/ods";
-
+    /** Folder for QA scripts. */
     public static String queriesFolder = "/queries/";
-
+    /** Folder for XML Schema files. */
     public static String schemaFolder = "/schema/";
-
+    /** Folder for XML files. */
     public static String xmlfileFolder = "xmlfile";
-
-    //full path to xmlfileFolder. Calclated at context init
+    /** Full path to xmlfileFolder. Calclated at context init. */
     public static String xmlfileFolderPath = "/xmlfile/";
 
-    public static final int CONV_SERVICE = 1; //Conversion service weight
-    public static final int QA_SERVICE = 2; //QA service weight
-    public static int services_installed = 3; //by default the both services are installed
+    /** Conversion service installation. */
+    public static final int CONV_SERVICE = 1;
+    /** QA service installation. */
+    public static final int QA_SERVICE = 2;
+    /** Number indicating the type of services are installed. Default is 3 (both Conv and QA). */
+    public static int services_installed = 3;
 
-    //public static String xformsFolder="/xforms/";
-
-    //Database settings from the properties file
+    /** DB URL. */
     public static String dbUrl = null;
+    /** DB driver class name. */
     public static String dbDriver = null;
+    /** DB connection user name. */
     public static String dbUser = null;
+    /** DB connection password. */
     public static String dbPwd = null;
 
-    //Edit UI
+    /** Edit UI folder. */
     public static String uiFolder = null;
 
-    //period for checking new jobs in the workqueue in milliseconds, default 20sec
-    public static long wqCheckInterval = 20000L;
-
-    //NB Saxon is the default value, not hard-coded!
-    public static String engineClass = "eionet.gdem.qa.engines.SaxonImpl";
-
-    //DCM settings from the properties file
+    /** conversion.xml file location, listing all available generated conversions. */
     public static String convFile = null;
+    /** XSL folder for generated conversions. */
     public static String metaXSLFolder = null;
+    /** Data Dictionary URL, used when generating XSLs. */
     public static String ddURL = null;
+    /** XMLCONV app URL. */
     public static String gdemURL = null;
-
-    //DCM settings from the properties file of incoming services from DD
+    /** Data Dictionary xml-rpc URL */
     public static String invServUrl = null;
+    /** Data Dictionary xml-rpc service name */
     public static String invServName = null;
-
-    //Content Registry SPARQL endpoint URL
+    /** Content Registry SPARQL endpoint URL. */
     public static String crSparqlEndpoint = null;
 
-    //ldap url
+    /** LDAP url. */
     public static String ldapUrl = null;
+    /** LDAP context. */
     public static String ldapContext = null;
+    /** LDAP user directory. */
     public static String ldapUserDir = null;
+    /** LDAP UID attribute. */
     public static String ldapAttrUid = null;
 
-    //DCM settings from the properties file of incoming services from CDR
-    //CDR doesn't use Service names
+    /** DCM settings from the properties file of incoming services from CDR. CDR doesn't use Service names. */
     public static String cdrServUrl = null;
 
-    // TODO add initialization from property file
+    /** OpenOffice port. */
     public static int openOfficePort = 8100;
+    /** Implementation class for QA queries. Saxon is the default value, not hard-coded. */
+    public static String engineClass = "eionet.gdem.qa.engines.SaxonImpl";
+    /** XGawk program executable command. */
     public static String xgawkCommand = null;
-    //timeout for running external QA program in command line in milliseconds, default 120sec
+    /** timeout for running external QA program in command line in milliseconds, default 120sec. */
     public static long qaTimeout = 120000L;
+    /** Maximum number of jobs executed in workqueue simultaniosly. */
+    public static int wqMaxJobs = 20;
+    /** period for checking new jobs in the workqueue in milliseconds, default 20sec. */
+    public static long wqCheckInterval = 20000L;
 
+    /** Resource bundle for gdem properties. */
     private static ResourceBundle props;
+    /** Resource bundle for LDAP properties. */
     private static ResourceBundle ldapProps;
-
+    /** Resource bundle for Messages. */
+    private static ResourceBundle applicationResources;
+    /** Logger class. */
     public static final Logger logger = Logger.getLogger(Properties.class);
 
-    public static String dateFormatPattern="dd MMM yyyy";
-    public static String timeFormatPattern="dd MMM yyyy hh:mm:ss";
+    /** Date pattern used for displaying date values on UI. */
+    public static String dateFormatPattern = "dd MMM yyyy";
+    /** Time pattern used for displaying time values on UI. */
+    public static String timeFormatPattern = "dd MMM yyyy hh:mm:ss";
 
-    private static ResourceBundle applicationResources;
-
-    public static int wqMaxJobs = 20;
-
-     static {
-
+    static {
 
         if (props == null) {
             props = ResourceBundle.getBundle("gdem");
             try {
                 queriesFolder = props.getString("queries.folder");
 
-                //xformsFolder=props.getString("xforms.folder");
+                // xformsFolder=props.getString("xforms.folder");
 
-                xslFolder=checkPath(props.getString("xsl.folder"));
-                tmpFolder=props.getString("tmp.folder");
-                odsFolder=checkPath(props.getString("ods.folder"));
+                xslFolder = checkPath(props.getString("xsl.folder"));
+                tmpFolder = props.getString("tmp.folder");
+                odsFolder = checkPath(props.getString("ods.folder"));
 
                 xmlfileFolder = props.getString("xmlfile.folder");
 
-                //DB connection settings
+                // DB connection settings
                 dbDriver = props.getString("db.driver");
                 dbUrl = props.getString("db.url");
                 dbUser = props.getString("db.user");
                 dbPwd = props.getString("db.pwd");
 
                 engineClass = props.getString("xq.engine.implementator");
-                //DCM settings
+                // DCM settings
                 ddURL = props.getString("dd.url");
                 gdemURL = props.getString("gdem.url");
 
-                //settings for incoming services from DD
+                // settings for incoming services from DD
                 invServUrl = props.getString("dd.rpc.url");
                 invServName = props.getString("dd.rpcservice.name");
 
-                //settings for incoming services from CDR
+                // settings for incoming services from CDR
                 cdrServUrl = props.getString("cdr.url");
 
-                //settings for incoming services from Content Registry
+                // settings for incoming services from Content Registry
                 crSparqlEndpoint = props.getString("cr.sparql.endpoint");
 
-                //period in seconds
+                // period in seconds
                 String frequency = props.getString("wq.check.interval");
                 Float f = new Float(frequency);
                 wqCheckInterval = (long) (f.floatValue() * 1000);
 
-                //maximum number of jobs executed at the same time
+                // maximum number of jobs executed at the same time
                 String maxJobs = props.getString("wq.max.jobs");
-                try{
-                    if (maxJobs!=null && maxJobs.length()>0) {wqMaxJobs =Integer.parseInt(maxJobs);}
+                try {
+                    if (maxJobs != null && maxJobs.length() > 0) {
+                        wqMaxJobs = Integer.parseInt(maxJobs);
+                    }
+                } catch (Exception e) {
+                    logger.error("\"wq.max.jobs\" property is not defined or is not numeric in gdem.properties");
                 }
-                catch(Exception e){}
 
-                dateFormatPattern=props.getString("date.format.pattern");
-                timeFormatPattern=props.getString("time.format.pattern");
+                dateFormatPattern = props.getString("date.format.pattern");
+                timeFormatPattern = props.getString("time.format.pattern");
 
                 try {
                     services_installed = Integer.parseInt(props.getString("gdem.services"));
-                } catch (Exception e) { //ignore, use default
+                } catch (Exception e) {
+                    logger.error("\"gdem.services\" property is not defined or is not numeric in gdem.properties");
                 }
 
                 String ooServicePortStr = props.getString("openoffice.service.port");
@@ -178,12 +192,13 @@ public class Properties {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                //maximum number of jobs executed at the same time
+                // maximum number of jobs executed at the same time
                 String sQaTimeout = props.getString("external.qa.timeout");
-                try{
-                    if (sQaTimeout!=null && sQaTimeout.length()>0) {qaTimeout =Long.parseLong(sQaTimeout);}
-                }
-                catch(Exception e){
+                try {
+                    if (sQaTimeout != null && sQaTimeout.length() > 0) {
+                        qaTimeout = Long.parseLong(sQaTimeout);
+                    }
+                } catch (Exception e) {
                     logger.error("\"external.qa.timeout\" property is not defined or is not numeric in gdem.properties");
                 }
                 xgawkCommand = props.getString("external.qa.command.xgawk");
@@ -191,7 +206,7 @@ public class Properties {
             } catch (MissingResourceException mse) {
                 logger.error("Missing property in gdem.properties");
                 mse.printStackTrace();
-                //no error handling? go with the default values??
+                // no error handling? go with the default values??
             } catch (Exception e) {
                 logger.error("Error when reading properties from gdem.properties");
                 e.printStackTrace();
@@ -210,22 +225,24 @@ public class Properties {
                 e.printStackTrace();
             }
         }
-        if (applicationResources == null){
+        if (applicationResources == null) {
             applicationResources = ResourceBundle.getBundle("ApplicationResources");
         }
 
     }
 
-    private static String checkPath(String path){
-        if(path.endsWith("/")){
-            path=path.substring(0, path.length() -1);
+    private static String checkPath(String path) {
+        if (path.endsWith("/")) {
+            path = path.substring(0, path.length() - 1);
         }
         return path;
     }
-    public static String getMessage(String key){
+
+    public static String getMessage(String key) {
         return applicationResources.getString(key);
     }
-    public static String getMessage(String key, Object[] replacement){
+
+    public static String getMessage(String key, Object[] replacement) {
         return MessageFormat.format(applicationResources.getString(key), replacement);
     }
 
--- src/eionet/gdem/qa/ListQueriesMethod.java
+++ src/eionet/gdem/qa/ListQueriesMethod.java
@@ -17,60 +17,76 @@ import eionet.gdem.services.db.dao.ISchemaDao;
 import eionet.gdem.utils.Utils;
 
 /**
- * Implementation of listQueries and listQAScripts methods
+ * Implementation of listQueries and listQAScripts methods.
  *
  * @author Enriko Käsper, TietoEnator Estonia AS
  */
 public class ListQueriesMethod extends RemoteServiceMethod {
 
-    public final static String KEY_QUERY_ID = "query_id";
-    public final static String KEY_QUERY = "query";
-    public final static String KEY_SHORT_NAME = "short_name";
-    public final static String KEY_DESCRIPTION = "description";
-    public final static String KEY_SCHEMA_ID = "schema_id";
-    public final static String KEY_XML_SCHEMA = "xml_schema";
-    public final static String KEY_TYPE = "type";
-    public final static String KEY_CONTENT_TYPE_OUT = "content_type_out";
-    public final static String KEY_CONTENT_TYPE_ID = "content_type_id";
-    public final static String KEY_UPPER_LIMIT = "upper_limit";
-    public final static int VALIDATION_UPPER_LIMIT = 50;
+    /** Query ID property key in ListQueries method result. */
+    public static final String KEY_QUERY_ID = "query_id";
+    /** Query file property key in ListQueries method result. */
+    public static final String KEY_QUERY = "query";
+    /** Query short name property key in ListQueries method result. */
+    public static final String KEY_SHORT_NAME = "short_name";
+    /** Query description property key in ListQueries method result. */
+    public static final String KEY_DESCRIPTION = "description";
+    /** Schema ID property key in ListQueries method result. */
+    public static final String KEY_SCHEMA_ID = "schema_id";
+    /** Schema URL property key in ListQueries method result. */
+    public static final String KEY_XML_SCHEMA = "xml_schema";
+    /** Type property key in ListQueries method result. */
+    public static final String KEY_TYPE = "type";
+    /** Output content type property key in ListQueries method result. */
+    public static final String KEY_CONTENT_TYPE_OUT = "content_type_out";
+    /** Output content type ID property key in ListQueries method result. */
+    public static final String KEY_CONTENT_TYPE_ID = "content_type_id";
+    /** XML file upper limit property key in ListQueries method result. */
+    public static final String KEY_UPPER_LIMIT = "upper_limit";
+    /** Upper limit for xml file size to be sent to manual QA. */
+    public static final int VALIDATION_UPPER_LIMIT = 50;
 
-    public final static String DEFAULT_CONTENT_TYPE_ID="HTML";
+    /** Default conversion output type. */
+    public static final String DEFAULT_CONTENT_TYPE_ID = "HTML";
 
+    /** DAO for getting schema info. */
     private ISchemaDao schemaDao = GDEMServices.getDaoService().getSchemaDao();;
+    /** DAO for getting query info. */
     private IQueryDao queryDao = GDEMServices.getDaoService().getQueryDao();
+    /** DAO for getting conversion types info. */
     private IConvTypeDao convTypeDao = GDEMServices.getDaoService().getConvTypeDao();
 
     /**
-     * List all possible QA scripts (XQueries, XML Schemas, DTD, XSLT?) for this XML Schema.
-     * If schema is null, then all possible QA scripts are returned
+     * List all possible QA scripts (XQueries, XML Schemas, DTD, XSLT?) for this XML Schema. If schema is null, then all possible QA
+     * scripts are returned
      *
-     * @param schema	URL of XML schema
-     * @return			returns array of Hastables with the following keys:	qyery_id, short_name,
-     * description, query, schema_id, xml_schema, content_type_out, type
+     * @param schema
+     *            URL of XML schema
+     * @return array of Hastables with the following keys: qyery_id, short_name, description, query, schema_id, xml_schema,
+     *         content_type_out, type
      *
      * @throws GDEMException
      */
     public Vector listQueries(String schema) throws GDEMException {
 
         Vector v = new Vector();
-        if (schema!=null && schema.equals(""))schema=null;
+        if (schema != null && schema.equals("")){
+            schema = null;
+        }
 
         try {
-            //Get schemas that has to be validated
-            Vector schemas=schemaDao.getSchemas(schema,false);
+            // Get schemas that has to be validated
+            Vector schemas = schemaDao.getSchemas(schema, false);
             Hashtable convType = convTypeDao.getConvType(DEFAULT_CONTENT_TYPE_ID);
-            String contentType=
-                (convType!=null && convType.containsKey("content_type"))?
-                        (String)convType.get("content_type"):
-                            DEFAULT_QA_CONTENT_TYPE;
+            String contentType = (convType != null && convType.containsKey("content_type")) ? (String) convType.get("content_type")
+                    : DEFAULT_QA_CONTENT_TYPE;
 
-            if (schemas!=null){
-                for (int i=0;i<schemas.size();i++){
-                    HashMap h = (HashMap)schemas.get(i);
-                    String validate = (String)h.get("validate");
-                    if (!Utils.isNullStr(validate)){
-                        if (validate.equals("1")){
+            if (schemas != null) {
+                for (int i = 0; i < schemas.size(); i++) {
+                    HashMap h = (HashMap) schemas.get(i);
+                    String validate = (String) h.get("validate");
+                    if (!Utils.isNullStr(validate)) {
+                        if (validate.equals("1")) {
                             Hashtable ht = new Hashtable();
                             ht.put(KEY_QUERY_ID, String.valueOf(Constants.JOB_VALIDATION));
                             ht.put(KEY_SHORT_NAME, "XML Schema Validation");
@@ -80,7 +96,7 @@ public class ListQueriesMethod extends RemoteServiceMethod {
                             ht.put(KEY_XML_SCHEMA, h.get("xml_schema"));
                             ht.put(KEY_CONTENT_TYPE_ID, DEFAULT_CONTENT_TYPE_ID);
                             ht.put(KEY_CONTENT_TYPE_OUT, contentType);
-                            ht.put(KEY_TYPE, ((String)h.get("schema_lang")).toLowerCase());
+                            ht.put(KEY_TYPE, ((String) h.get("schema_lang")).toLowerCase());
                             ht.put(KEY_UPPER_LIMIT, String.valueOf(VALIDATION_UPPER_LIMIT));
                             v.add(ht);
                         }
@@ -88,24 +104,24 @@ public class ListQueriesMethod extends RemoteServiceMethod {
                 }
             }
             // Get XQueries
-            Vector queries=queryDao.listQueries(schema);
-            if (queries!=null){
-                for (int i=0;i<queries.size();i++){
-                    Hashtable ht = (Hashtable)queries.get(i);
+            Vector queries = queryDao.listQueries(schema);
+            if (queries != null) {
+                for (int i = 0; i < queries.size(); i++) {
+                    Hashtable ht = (Hashtable) queries.get(i);
                     ht.put(KEY_TYPE, Constants.QA_TYPE_XQUERY);
-                    //return full URL of XQuerys
-                    ht.put(KEY_QUERY, Properties.gdemURL + "/" + Constants.QUERIES_FOLDER + (String)ht.get("query"));
+                    // return full URL of XQuerys
+                    ht.put(KEY_QUERY, Properties.gdemURL + "/" + Constants.QUERIES_FOLDER + (String) ht.get("query"));
                     v.add(ht);
                 }
             }
-        } catch (Exception e ) {
+        } catch (Exception e) {
             throw new GDEMException("Error getting data from the DB " + e.toString(), e);
         }
         return v;
     }
+
     /**
-     * List all  XQueries and their modification times for this namespace
-     * returns also XML Schema validation.
+     * List all XQueries and their modification times for this namespace returns also XML Schema validation.
      *
      * @param schema
      * @return result is an Array of Arrays that contains 3 fields (script_id, description, last modification)
@@ -115,14 +131,16 @@ public class ListQueriesMethod extends RemoteServiceMethod {
         Vector<Vector<String>> result = new Vector<Vector<String>>();
         Vector<String> resultQuery = null;
         try {
-            Vector v=schemaDao.getSchemas(schema);
+            Vector v = schemaDao.getSchemas(schema);
 
-            if (Utils.isNullVector(v)) return result;
+            if (Utils.isNullVector(v)){
+                return result;
+            }
 
-            HashMap h = (HashMap)v.get(0);
-            String validate = (String)h.get("validate");
-            if (!Utils.isNullStr(validate)){
-                if (validate.equals("1")){
+            HashMap h = (HashMap) v.get(0);
+            String validate = (String) h.get("validate");
+            if (!Utils.isNullStr(validate)) {
+                if (validate.equals("1")) {
                     resultQuery = new Vector<String>();
                     resultQuery.add(String.valueOf(Constants.JOB_VALIDATION));
                     resultQuery.add("XML Schema Validation");
@@ -131,40 +149,40 @@ public class ListQueriesMethod extends RemoteServiceMethod {
                     result.add(resultQuery);
                 }
             }
-            Vector queries = (Vector)h.get("queries");
-            if (Utils.isNullVector(queries)) return result;
+            Vector queries = (Vector) h.get("queries");
+            if (Utils.isNullVector(queries))
+                return result;
 
-            for (int i = 0; i <queries.size(); i++){
-                HashMap hQueries = (HashMap)queries.get(i);
-                String queryId = (String)hQueries.get("query_id");
-                String queryFile = (String)hQueries.get("query");
-                String queryDescription = (String)hQueries.get("descripton");
-                String queryName = (String)hQueries.get("short_name");
-                String queryUpperLimit = (String)hQueries.get("upper_limit");
-                if (Utils.isNullStr(queryDescription)){
-                    if (Utils.isNullStr(queryName)){
+            for (int i = 0; i < queries.size(); i++) {
+                HashMap hQueries = (HashMap) queries.get(i);
+                String queryId = (String) hQueries.get("query_id");
+                String queryFile = (String) hQueries.get("query");
+                String queryDescription = (String) hQueries.get("descripton");
+                String queryName = (String) hQueries.get("short_name");
+                String queryUpperLimit = (String) hQueries.get("upper_limit");
+                if (Utils.isNullStr(queryDescription)) {
+                    if (Utils.isNullStr(queryName)) {
                         queryDescription = "Quality Assurance script";
-                    }
-                    else {
-                        queryDescription=queryName;
+                    } else {
+                        queryDescription = queryName;
                     }
                 }
                 resultQuery = new Vector<String>();
                 resultQuery.add(queryId);
                 resultQuery.add(queryDescription);
-                File f=new File(Properties.queriesFolder + queryFile);
-                String last_modified="";
+                File f = new File(Properties.queriesFolder + queryFile);
+                String last_modified = "";
 
-                if (f!=null)
-                    last_modified = Utils.getDateTime(new Date(f.lastModified()));;
+                if (f != null) {
+                    last_modified = Utils.getDateTime(new Date(f.lastModified()));
+                }
 
-                    resultQuery.add(last_modified);
-                    resultQuery.add(queryUpperLimit);
-                    result.add(resultQuery);
+                resultQuery.add(last_modified);
+                resultQuery.add(queryUpperLimit);
+                result.add(resultQuery);
             }
 
-
-        } catch (Exception e ) {
+        } catch (Exception e) {
             throw new GDEMException("Error getting data from the DB " + e.toString(), e);
         }
 
