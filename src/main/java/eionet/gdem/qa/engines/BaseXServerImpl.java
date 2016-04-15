9a933c01da1b9a552b7a6d3b7ff157a6aa178fba
George Sofianos <gso@eworx.gr>
2016-04-15 15:32:48 +0300

    Added basex implementation

---
+++ src/main/java/eionet/gdem/qa/engines/BaseXServerImpl.java
@@ -0,0 +1,18 @@
+package eionet.gdem.qa.engines;
+
+import eionet.gdem.GDEMException;
+import eionet.gdem.qa.XQScript;
+
+import java.io.OutputStream;
+
+/**
+ * @author George Sofianos
+ *
+ */
+public class BaseXServerImpl extends QAScriptEngineStrategy {
+
+    @Override
+    protected void runQuery(XQScript script, OutputStream result) throws GDEMException {
+
+    }
+}
