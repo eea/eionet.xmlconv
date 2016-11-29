3277b101e580f03e4bc54afdd646caca002abf92
George Sofianos <gso@eworx.gr>
2016-11-29 19:00:57 +0200

    Small update. Refs #79393

--- src/main/java/eionet/gdem/utils/HttpUtils.java
+++ src/main/java/eionet/gdem/utils/HttpUtils.java
@@ -83,7 +83,6 @@ public final class HttpUtils {
         } finally {
             response.close();
             method.releaseConnection();
-            client.close();
         }
         return responseBody;
     }
