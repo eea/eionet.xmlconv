5c5d32e5c255abf754861812d12188359cc564d3
Aris Katsanas <aka@eworx.gr>
2017-04-05 10:07:20 +0300

    Implementation of filtering by Status on the Workqueue Table, Refs #83431

---
+++ src/main/webapp/scripts/workqueue.js
@@ -0,0 +1,82 @@
+/**
+ * Created by dev_aka on 4/4/2017.
+ */
+$(document).ready(function() {
+
+    const LOCAL_STORAGE_KEY = 'workqueueFilter';
+
+    var filter = {
+        received : { selected: true , text: 'JOB'},
+        processing: { selected: true , text: 'PROCESSING'},
+        ready: { selected: true , text: 'READY'},
+        error: { selected: true , text: 'FATAL'}
+    };
+
+    var initFilters = function () {
+        if ( sessionStorage[LOCAL_STORAGE_KEY] ) {
+            try {
+                var tmp = JSON.parse(sessionStorage[LOCAL_STORAGE_KEY]);
+                for (var i in tmp) {
+                    if (!tmp[i].selected)
+                        $("#" + i).click();
+                }
+            } catch (e) {
+                sessionStorage[LOCAL_STORAGE_KEY] = null;
+            }
+        }
+    };
+
+    // initialize the Datatable
+    var table = $('#workqueue_table').DataTable( {
+        paging:   false,
+        ordering: false,
+        info:     true,
+        bAutoWidth: false,
+    } );
+
+    // registering dropdown listeners
+    $('.dropdown-container')
+        .on('click', '.dropdown-button', function(e) {
+            $('.dropdown-content').toggle();
+        })
+
+        .on('click', function (e) {
+            e.stopPropagation();
+        })
+
+        .on('change', '[type="checkbox"]', function(e) {
+
+            var status = this.name;
+            var temp = [];
+
+            filter[status].selected = ! filter[status].selected;
+
+            for ( var i in filter){
+                if (filter[i].selected) {
+                    temp.push(filter[i].text)
+                }
+            }
+
+            if ( temp.length === 4 || temp.length === 0){
+                $('.fa-filter').css({ 'color': '#5f646f' })
+                table.column( 5 ).search ( '' ) . draw ()
+            }
+            else{
+                $('.fa-filter').css({ 'color': 'blue' });
+                table.column( 5 ).search ( '(' + temp.join('|') + ')' , true ) . draw ();
+            }
+
+            sessionStorage[LOCAL_STORAGE_KEY] = JSON.stringify(filter);
+
+            e.stopPropagation();
+
+        });
+
+    /* on outside clicks hide the dropdown */
+    $(document).click(function(){
+        $('.dropdown-content').hide();
+    });
+
+    initFilters(); // after having registered the listeners
+
+} );
