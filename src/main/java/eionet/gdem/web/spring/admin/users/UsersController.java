c967017c253acac97e9f33faf85b7922240693c2
George Sofianos <gso@eworx.gr>
2018-09-17 17:22:19 +0300

    Small update. Refs #96287

--- src/main/java/eionet/gdem/web/spring/admin/users/UsersController.java
+++ src/main/java/eionet/gdem/web/spring/admin/users/UsersController.java
@@ -1,16 +1,20 @@
 package eionet.gdem.web.spring.admin.users;
 
 import eionet.acl.SignOnException;
+import eionet.gdem.Constants;
 import eionet.gdem.security.acl.AccessListService;
 import eionet.gdem.services.MessageService;
+import eionet.gdem.utils.SecurityUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpSession;
 import java.sql.SQLException;
 import java.util.*;
 
@@ -29,7 +33,12 @@ public class UsersController {
     }
 
     @GetMapping
-    public String list(Model model) throws SignOnException, SQLException {
+    public String list(Model model, HttpSession httpSession) throws SignOnException, SQLException {
+
+        String user = (String) httpSession.getAttribute("user");
+        if (!SecurityUtil.hasPerm(user, "/" + Constants.ACL_CONFIG_PATH, "u")) {
+            throw new AccessDeniedException(messageService.getMessage("label.autorization.config.update"));
+        }
 
         HashMap<String, List<String>> groups = accessListService.getGroups();
         model.addAttribute("groups", groups);
@@ -38,14 +47,25 @@ public class UsersController {
     }
 
     @PostMapping
-    public String submit(UsersForm form) throws SignOnException {
+    public String submit(UsersForm form, HttpSession httpSession) throws SignOnException {
+        String user = (String) httpSession.getAttribute("user");
+        if (!SecurityUtil.hasPerm(user, "/" + Constants.ACL_CONFIG_PATH, "u")) {
+            throw new AccessDeniedException(messageService.getMessage("label.autorization.config.update"));
+        }
+
         List<Group> groups = form.getGroups();
         accessListService.writeGroups(groups);
         return "redirect:/admin/users";
     }
 
     @GetMapping("/edit")
-    public String edit(Model model) throws SQLException, SignOnException {
+    public String edit(Model model, HttpSession httpSession) throws SQLException, SignOnException {
+
+        String user = (String) httpSession.getAttribute("user");
+        if (!SecurityUtil.hasPerm(user, "/" + Constants.ACL_CONFIG_PATH, "u")) {
+            throw new AccessDeniedException(messageService.getMessage("label.autorization.config.update"));
+        }
+
         HashMap<String, List<String>> groups = accessListService.getGroups();
         model.addAttribute("groups", groups);
         model.addAttribute("form", new UsersForm());
