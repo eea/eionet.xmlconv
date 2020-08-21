f84148c4d7e3703a21a75d68f62d74e69b47b54a
deniaAthanasakou <deniaAthanasakou>
2020-08-21 16:05:22 +0300

    Created scheduled task for notifications. Refs #113560

--- src/main/java/eionet/gdem/infrastructure/scheduling/FixedTimeScheduledTasks.java
+++ src/main/java/eionet/gdem/infrastructure/scheduling/FixedTimeScheduledTasks.java
@@ -43,25 +43,29 @@ public class FixedTimeScheduledTasks {
         Map<String, Timestamp> jobsInfo = xqJobDao.getJobsWithTimestamps(Constants.XQ_PROCESSING);
         //Create new map with the duration for each job
         Map<String, Long> jobDurations = new HashMap<>();
-        List<String> longRunningJobIds = new ArrayList<>();
         for (Map.Entry<String,Timestamp> entry : jobsInfo.entrySet()) {
             Long currentMs = new Timestamp(new Date().getTime()).getTime();
             long diffInMs = Math.abs(currentMs - entry.getValue().getTime());
-            if(diffInMs >= Properties.longRunningJobThreshold){
-                longRunningJobIds.add(entry.getKey());
-            }
             jobDurations.put(entry.getKey(), diffInMs);
-
             //Update time spent in status in table JOB_HISTORY
             repository.setDurationForJobHistory(diffInMs, entry.getKey(), Constants.XQ_PROCESSING);
         }
         //Update time spent in status in table T_XQJOBS
         xqJobDao.updateXQJobsDuration(jobDurations);
+        LOGGER.info("Updated duration of jobs in PROCESSING status.");
+    }
+
+    @Transactional
+    @Scheduled(cron = "0 * */4 * * *") //Every 4 hours
+    public void schedulePeriodicNotificationsForLongRunningJobs() throws SQLException, GeneralSecurityException {
+        //Retrieve jobs from T_XQJOBS with status PROCESSING (XQ_PROCESSING = 2) and duration more than Properties.LONG_RUNNING_JOBS_EVENT
+        String[] jobsIds = xqJobDao.getLongRunningJobs(Properties.longRunningJobThreshold, Constants.XQ_PROCESSING);
+        List<String> longRunningJobIds = Arrays.asList(jobsIds);
         if(longRunningJobIds.size() > 0){
             LOGGER.info("Found long running jobs with ids " + longRunningJobIds);
             //send notifications to users via UNS
             new UNSEventSender().longRunningJobsNotifications(longRunningJobIds, Properties.LONG_RUNNING_JOBS_EVENT);
+            LOGGER.info("Sent notifications for long running jobs");
         }
-        LOGGER.info("Updated duration of jobs in PROCESSING status.");
     }
 }
