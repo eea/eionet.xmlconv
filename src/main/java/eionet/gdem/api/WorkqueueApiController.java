e219eb14b49ca1903fe014b9bb7296d13b9c75ec
deniaAthanasakou <deniaAthanasakou>
2020-09-25 14:49:25 +0300

    Began implementing tests for job cancellation endpoint. Refs #121689

--- src/main/java/eionet/gdem/api/WorkqueueApiController.java
+++ src/main/java/eionet/gdem/api/WorkqueueApiController.java
@@ -2,12 +2,14 @@ package eionet.gdem.api;
 
 import eionet.gdem.Properties;
 import eionet.gdem.api.errors.BadRequestException;
+import eionet.gdem.utils.Utils;
 import eionet.gdem.web.spring.workqueue.WorkqueueManager;
 import org.apache.commons.lang.time.StopWatch;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,37 +22,28 @@ public class WorkqueueApiController {
     private static final Logger LOGGER = LoggerFactory.getLogger(WorkqueueApiController.class);
     /** Job ID parameter name */
     protected static final String JOB_ID_PARAM = "job_id";
-    /** Job deletion token parameter name */
-    protected static final String TOKEN_PARAM = "job_deletion_token";
 
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
     public void delete(HttpServletRequest request, HttpServletResponse response) {
         StopWatch timer = new StopWatch();
         timer.start();
         String jobId = null;
-        String authenticationToken = null;
         try {
-            /* The request method should be DELETE*/
-            if (request.getMethod() != "POST") {
-                throw new BadRequestException("The request method was not POST.");
+            /* The request method should be POST*/
+            if (!request.getMethod().equals("POST")) {
+                LOGGER.error("The request method was not POST.");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().write("\"{\"message\":\"Method was not POST\"}\"");
             }
             Map params = request.getParameterMap();
             // parse request parameters
-            if (params.containsKey(TOKEN_PARAM)) {
-                authenticationToken = (String) ((Object[]) params.get(TOKEN_PARAM))[0];
-            }
-            else{
-                throw new BadRequestException("No authentication token was provided for job deletion via API.");
-            }
             if (params.containsKey(JOB_ID_PARAM)) {
                 jobId = (String) ((Object[]) params.get(JOB_ID_PARAM))[0];
             }
             else{
-                throw new BadRequestException("No job id was provided for job deletion via API.");
-            }
-
-            /* Check validity of token */
-            if (authenticationToken != Properties.JOB_DELETION_AUTHENTICATION_TOKEN){
-                throw new BadRequestException("Wrong token was provided for job deletion via API.");
+                LOGGER.error("No job id was provided for job deletion via API.");
+                response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                response.getWriter().write("\"{\"message\":\"Missing job id from request\"}\"");
             }
             LOGGER.info("Deleting job via API with id " + jobId);
 
@@ -59,11 +52,15 @@ public class WorkqueueApiController {
             jobIds[0] = jobId;
             WorkqueueManager workqueueManager = new WorkqueueManager();
 
+
             workqueueManager.deleteJobs(jobIds);
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getWriter().write("\"{\"message\":\"Job deleted successfully\"}\"");
             timer.stop();
             LOGGER.info(String.format("Deleting of job #%s via API was completed, total time of execution: %s", jobId, timer.toString()));
         } catch (Exception e) {
             LOGGER.error(e.getMessage());
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
         }
     }
 }
