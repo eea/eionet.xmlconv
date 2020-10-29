f4958223a11b9a576c5647e4d4aa843cb3b0f5b2
Maria Favva <maria.favva@gmail.com>
2020-10-29 14:32:44 +0200

    extracted methods. Refs#111092

--- src/main/java/eionet/gdem/api/qa/web/FileDownloadController.java
+++ src/main/java/eionet/gdem/api/qa/web/FileDownloadController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 
 @RestController
@@ -31,7 +32,7 @@ public class FileDownloadController {
     @RequestMapping(value = "/downloadFile/{fileName}", method = RequestMethod.GET)
     public String getFile(@PathVariable String fileName, @RequestHeader("authorization") String authorization) throws JWTException, IOException {
         String filePath = null;
-        String urlPath = "/" + Constants.TMP_FOLDER + fileName + Constants.HTML_FILE;
+        String urlPath = new StringBuilder("/tmp/").append(fileName).append(Constants.HTML_FILE).toString();
 
         String rawAuthenticationToken = authorization;
         String parsedAuthenticationToken = authTokenService.getParsedAuthenticationTokenFromSchema(rawAuthenticationToken, Properties.jwtHeaderSchema);
@@ -40,8 +41,8 @@ public class FileDownloadController {
         }
 
         File file = new File(filePath);
-        byte[] bytes = Files.readAllBytes(file.toPath());
-        return new String(bytes, "UTF-8");
+        byte[] bytes = readFile(file);
+        return convertBytesToString(bytes);
     }
 
     protected String getFilePath(String urlPath, String fileName) {
@@ -51,6 +52,14 @@ public class FileDownloadController {
             throw new IllegalArgumentException();
         }
         return filePath;
+    }
+
+    protected byte[] readFile(File file) throws IOException {
+        return Files.readAllBytes(file.toPath());
+    }
+
+    protected String convertBytesToString(byte[] bytes) throws UnsupportedEncodingException {
+        return new String(bytes, "UTF-8");
     }
 
     @ExceptionHandler(ServletRequestBindingException.class)
