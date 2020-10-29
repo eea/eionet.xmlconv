d2dc586c6475a9b6d2401927e065ef999972c78a
Maria Favva <maria.favva@gmail.com>
2020-10-29 18:16:44 +0200

    fixing endpoint for downloading zip file. Refs#111092

--- src/main/java/eionet/gdem/api/qa/web/FileDownloadController.java
+++ src/main/java/eionet/gdem/api/qa/web/FileDownloadController.java
@@ -1,23 +1,22 @@
 package eionet.gdem.api.qa.web;
 
-import eionet.gdem.Constants;
 import eionet.gdem.Properties;
 import eionet.gdem.security.errors.JWTException;
 import eionet.gdem.security.service.AuthTokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.File;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
+@RequestMapping("/download")
 public class FileDownloadController {
 
     AuthTokenService authTokenService;
@@ -29,20 +28,27 @@ public class FileDownloadController {
         this.authTokenService = authTokenService;
     }
 
-    @RequestMapping(value = "/downloadFile/{fileName}", method = RequestMethod.GET)
-    public String getFile(@PathVariable String fileName, @RequestHeader("authorization") String authorization) throws JWTException, IOException {
+
+    @RequestMapping(value = "/zip/{fileName}", method = RequestMethod.GET)
+    public void getFile(@PathVariable String fileName, @RequestHeader(value = "authorization") String authorization, HttpServletResponse response) throws JWTException, IOException {
         String filePath = null;
-        String urlPath = new StringBuilder("/tmp/").append(fileName).append(Constants.HTML_FILE).toString();
+        String urlPath = new StringBuilder("/tmp/").append(fileName).append(".zip").toString();
 
         String rawAuthenticationToken = authorization;
         String parsedAuthenticationToken = authTokenService.getParsedAuthenticationTokenFromSchema(rawAuthenticationToken, Properties.jwtHeaderSchema);
         if (authTokenService.verifyUser(parsedAuthenticationToken)) {
-            filePath = getFilePath(urlPath, fileName);
+           filePath = getFilePath(urlPath, fileName);
         }
 
-        File file = new File(filePath);
-        byte[] bytes = readFile(file);
-        return convertBytesToString(bytes);
+        Path file = Paths.get(filePath);
+        if (Files.exists(file)) {
+            response.setContentType("application/zip");
+            response.addHeader("Content-Disposition", "attachment; filename=" + file.getFileName());
+            Files.copy(file, response.getOutputStream());
+            response.getOutputStream().flush();
+        } else {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
 
     protected String getFilePath(String urlPath, String fileName) {
@@ -54,36 +60,28 @@ public class FileDownloadController {
         return filePath;
     }
 
-    protected byte[] readFile(File file) throws IOException {
-        return Files.readAllBytes(file.toPath());
-    }
-
-    protected String convertBytesToString(byte[] bytes) throws UnsupportedEncodingException {
-        return new String(bytes, "UTF-8");
-    }
-
     @ExceptionHandler(ServletRequestBindingException.class)
-    public ResponseEntity<String> handleServletRequestBindingException(Exception exception) {
+    public void handleServletRequestBindingException(Exception exception, HttpServletResponse response) throws IOException {
         LOGGER.info(exception.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing authorization token.");
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Missing authorization token");
     }
 
     @ExceptionHandler(JWTException.class)
-    public ResponseEntity<String> handleJWTException(Exception exception) {
+    public void handleJWTException(Exception exception, HttpServletResponse response) throws IOException {
         LOGGER.info("Error during token verification.",exception);
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.getMessage());
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, exception.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<String> handleIllegalArgumentException(Exception exception) {
+    public void handleIllegalArgumentException(Exception exception, HttpServletResponse response) throws IOException {
         LOGGER.info("Got an IllegalArgumentException from user code; interpreting it as 400 Bad Request.", exception);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
     }
 
     @ExceptionHandler(IOException.class)
-    public ResponseEntity<String> handleIOException(Exception exception) {
-        LOGGER.info("Error during file reading", exception);
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error during file reading");
+    public void handleIOException(Exception exception, HttpServletResponse response) throws IOException {
+        LOGGER.info("Error",exception);
+        response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
     }
 }
 
