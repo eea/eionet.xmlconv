3a436ea2bb1b65252e6c67659a291df0f68d1bd5
Maria Favva <maria.favva@gmail.com>
2020-11-25 17:43:08 +0200

    removed logger. Refs#124446

--- src/main/java/eionet/gdem/rabbitMQ/WorkersJobsResultsMessageReceiver.java
+++ src/main/java/eionet/gdem/rabbitMQ/WorkersJobsResultsMessageReceiver.java
@@ -1,14 +1,8 @@
 package eionet.gdem.rabbitMQ;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 public class WorkersJobsResultsMessageReceiver {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(WorkersJobsResultsMessageReceiver.class);
-
     public void handleMessage(String message) {
-        LOGGER.info("Message Received: " + message);
         System.out.println("Message Received: " + message);
     }
 }
