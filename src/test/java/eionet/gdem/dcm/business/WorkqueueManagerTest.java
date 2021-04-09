f11e3469baccb89d22a91b96865a4353c0daa39a
vskiadas90 <vskiadas90@gmail.com>
2021-04-09 12:07:51 +0300

    - added delay for failing test due to premature verification

--- src/test/java/eionet/gdem/dcm/business/WorkqueueManagerTest.java
+++ src/test/java/eionet/gdem/dcm/business/WorkqueueManagerTest.java
@@ -81,6 +81,7 @@ public class WorkqueueManagerTest {
         WorkqueueManager wqm = new WorkqueueManager();
 
         String jobId = wqm.addQAScriptToWorkqueue(user, sourceUrl, scriptContent, scriptType);
+        Thread.sleep(4000L);
         WorkqueueJob job = wqm.getWqJob(jobId);
         String contentFile = job.getScriptFile();
         String content = Utils.readStrFromFile(contentFile);
@@ -89,7 +90,8 @@ public class WorkqueueManagerTest {
         assertEquals(2, job.getStatus());
         assertEquals("0", job.getScriptId());
         assertEquals(content, scriptContent);
-    }
+    }  //  WorkqueueManagerTest.testAddQAScriptToWorkqueue:89 expected:<2> but was:<0>
+
 
     /**
      * The method adds several jobs to workqueue
