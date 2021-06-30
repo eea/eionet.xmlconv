29ce3af0778ec9cbdf6a43836136aaf53f845ca2
vskiadas90 <vskiadas90@gmail.com>
2021-06-30 18:03:29 +0300

    - dockerfile upgrades in regards to goreplay mirroring

--- goreplay.sh
+++ goreplay.sh
@@ -3,4 +3,4 @@
  cd /
  tar -xzf gor_v1.2.0_x64.tar.gz
  chmod +x gor
- ./gor --input-raw :8080 --output-http=https://converterstest.eionet.europa.eu  --http-set-header "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZW1vX3VzZXIiLCJhdWQiOiJlZWEiLCJpc3MiOiJlZWEifQ.ZCtBmOKkiWnukXaKuw2wk3DSDgL9cHCUYJi2wXyEtQo"
+ ./gor --input-raw :8080 --output-http=http://converters.devel6cph.eea.europa.eu --http-set-header "Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkZW1vX3VzZXIiLCJhdWQiOiJlZWEiLCJpc3MiOiJlZWEifQ.ZCtBmOKkiWnukXaKuw2wk3DSDgL9cHCUYJi2wXyEtQo"
