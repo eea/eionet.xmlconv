7c4ec2074f06325555d867981e815f0a969544cd
deniaAthanasakou <antonia.ath@gmail.com>
2021-08-23 13:23:08 +0300

    Replaced hard coded localhost with property.

--- src/main/java/eionet/gdem/http/HttpFileManager.java
+++ src/main/java/eionet/gdem/http/HttpFileManager.java
@@ -1,6 +1,7 @@
 package eionet.gdem.http;
 
 import eionet.gdem.Constants;
+import eionet.gdem.Properties;
 import eionet.gdem.XMLConvException;
 import eionet.gdem.services.GDEMServices;
 import eionet.gdem.web.spring.hosts.IHostDao;
@@ -132,16 +133,21 @@ public class HttpFileManager {
              * XXX: the purpose of the next line is to bypass any load balancer. Load balancers can have small timeouts which
              * can result in broken http transfers. We obviously need a better solution.
              */
-                url.append("http://localhost:8080");
-                url.append(Constants.GETSOURCE_URL);
-                url.append("?");
-                url.append(Constants.TICKET_PARAM);
-                url.append("=");
-                url.append(ticket);
-                url.append("&");
-                url.append(Constants.SOURCE_URL_PARAM);
-                url.append("=");
-                url.append(parsedURL);
+            if(!Utils.isNullStr(Properties.HTTP_TRANSFER_LOADBALANCER)){
+                url.append(Properties.HTTP_TRANSFER_LOADBALANCER);
+            }
+            else{
+                url.append(Properties.gdemURL);
+            }
+            url.append(Constants.GETSOURCE_URL);
+            url.append("?");
+            url.append(Constants.TICKET_PARAM);
+            url.append("=");
+            url.append(ticket);
+            url.append("&");
+            url.append(Constants.SOURCE_URL_PARAM);
+            url.append("=");
+            url.append(parsedURL);
         } else {
             url.append(parsedURL);
         }
