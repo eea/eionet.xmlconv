59161827d0496829d8c48f87a4397e9fe99dc85c
Maria Favva <maria.favva@gmail.com>
2021-09-24 15:13:25 +0300

    implementation for heavy jobs and refactor. Refs#139338

--- src/main/java/eionet/gdem/rabbitMQ/service/RabbitMQMessageSender.java
+++ src/main/java/eionet/gdem/rabbitMQ/service/RabbitMQMessageSender.java
@@ -1,29 +1,10 @@
 package eionet.gdem.rabbitMQ.service;
 
-import eionet.gdem.rabbitMQ.model.WorkerHeartBeatMessageInfo;
 import eionet.gdem.rabbitMQ.model.WorkerInfo;
-import eionet.gdem.rabbitMQ.model.WorkerJobRabbitMQRequest;
 
 public interface RabbitMQMessageSender<T extends WorkerInfo> {
 
 
     void sendMessageToRabbitMQ(T t);
-//    /**
-//     * sends job to light worker
-//     * @param workerJobRabbitMQRequest
-//     */
-//    void sendJobInfoToRabbitMQ(WorkerJobRabbitMQRequest workerJobRabbitMQRequest);
-//
-//    /**
-//     * sends heart beat messages to workers
-//     * @param workerHeartBeatMessageInfo
-//     */
-//    void sendHeartBeatMessage(WorkerHeartBeatMessageInfo workerHeartBeatMessageInfo);
-
-//    /**
-//     * sends job to heavy worker
-//     * @param workerJobRabbitMQRequest
-//     */
-//    void sendJobInfoToHeavyRabbitmqQueue(WorkerJobRabbitMQRequest workerJobRabbitMQRequest);
 
 }
