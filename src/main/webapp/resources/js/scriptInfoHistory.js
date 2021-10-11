0f996166768fa7af3a6f688ddedd710ab5de5639
Maria Favva <maria.favva@gmail.com>
2021-10-11 14:17:10 +0300

    removed id column from script history table. Refs#139574

--- src/main/webapp/resources/js/scriptInfoHistory.js
+++ src/main/webapp/resources/js/scriptInfoHistory.js
@@ -11,7 +11,6 @@ var app = new Vue({
             search: '',
             loading: true,
             headers: [
-                {text: "id", value: "id"},
                 {text: "Upper limit", value: "upperLimit"},
                 {text: "Url", value: "url"},
                 {text: "Active", value: "active"},
