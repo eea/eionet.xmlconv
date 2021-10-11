ce4f5e73d8308deac267378d9873cde456207393
vskiadas90 <vskiadas90@gmail.com>
2021-10-11 11:46:13 +0300

    - enhanced comment in Heavy Workers Jobs Identification

--- src/main/java/eionet/gdem/rabbitMQ/listeners/WorkersJobsResultsMessageReceiver.java
+++ src/main/java/eionet/gdem/rabbitMQ/listeners/WorkersJobsResultsMessageReceiver.java
@@ -98,7 +98,7 @@ public class WorkersJobsResultsMessageReceiver implements MessageListener {
         if (response.getJobExecutorType().equals(JobExecutorType.Heavy)) {  //the response came from a heavy worker, so the job is heavy
             jobEntry.setHeavy(true);
             List<JobExecutor> jobExecutors = jobExecutorService.findExecutorsByJobId(jobEntry.getId());
-            //find light workers that may have grabbed the job before the job was sent to heavy queue and set their status to failed
+            //find light workers that may have grabbed the job before the job was sent to heavy queue and set their status to failed in order for them to be deleted later by the responsible scheduled Task.
             jobExecutors = jobExecutors.stream().filter(j -> j.getJobExecutorType() == JobExecutorType.Light).collect(Collectors.toList());
             if (jobExecutors.size()!=0) {
                 for (JobExecutor lightJobExecutor : jobExecutors) {
