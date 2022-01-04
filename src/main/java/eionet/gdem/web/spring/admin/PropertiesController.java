88a96f4a79c19c26b6e60126b82d5344ce86f26d
Maria Favva <maria.favva@gmail.com>
2022-01-04 13:30:59 +0200

    fixes for login error problem. Refs#143359

--- src/main/java/eionet/gdem/web/spring/admin/PropertiesController.java
+++ src/main/java/eionet/gdem/web/spring/admin/PropertiesController.java
@@ -3,12 +3,11 @@ package eionet.gdem.web.spring.admin;
 import eionet.acl.SignOnException;
 import eionet.gdem.Constants;
 import eionet.gdem.Properties;
-import eionet.gdem.jpa.errors.DatabaseException;
 import eionet.gdem.services.MessageService;
 import eionet.gdem.utils.SecurityUtil;
 import eionet.gdem.utils.ThymeleafUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.nio.file.AccessDeniedException;
 
 @Controller
 @RequestMapping("/admin/viewAndEditProperties")
@@ -29,17 +29,17 @@ public class PropertiesController {
     }
 
     @GetMapping
-    public String view(Model model, HttpServletRequest request) throws SignOnException, DatabaseException {
-        String user = (String) request.getSession().getAttribute("user");
-        if (!SecurityUtil.hasPerm(user, "/" + Constants.ACL_ADMIN_PATH, "u")) {
-            throw new AccessDeniedException(messageService.getMessage("label.authorization.properties.view"));
-        }
-
+    public String view(Model model, HttpSession session, HttpServletRequest request) throws SignOnException, AccessDeniedException {
         //Setup headerVariables
         model = ThymeleafUtils.setUpTitleAndLogin(model, Properties.getStringProperty("label.admin.properties"), request);
         //Setup breadcrumbs
         model = ThymeleafUtils.setUpBreadCrumbsForAdminPages(model, Properties.getStringProperty("label.admin.properties"));
 
+        String user = (String) session.getAttribute("user");
+        if (user==null || (user!=null && !SecurityUtil.hasPerm(user, "/" + Constants.ACL_ADMIN_PATH, "u"))) {
+            model.addAttribute("reason", HttpStatus.UNAUTHORIZED.value() + " - " + messageService.getMessage("label.authorization.properties.view"));
+            return "error/error";
+        }
         return "/old/admin/html/viewAndEditProperties";
     }
 }
