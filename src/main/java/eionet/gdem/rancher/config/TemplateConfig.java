4e4b350a131b1f0ccfef587880c16b71bb8a96a4
Maria Favva <maria.favva@gmail.com>
2022-02-11 11:32:56 +0200

    added extra circuit breaker configuration. Refs#143459

--- src/main/java/eionet/gdem/rancher/config/TemplateConfig.java
+++ src/main/java/eionet/gdem/rancher/config/TemplateConfig.java
@@ -45,7 +45,9 @@ public class TemplateConfig {
     @Bean
     public CircuitBreaker circuitBreaker() {
         CircuitBreakerConfig config = CircuitBreakerConfig.custom()
-                .minimumNumberOfCalls(1)     //if 1 call fails, circuit breaker will open
+                .minimumNumberOfCalls(1)     //if 1 call fails in the last 60 seconds, circuit breaker will open
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.TIME_BASED)
+                .slidingWindowSize(60)
                 .waitDurationInOpenState(Duration.ofMinutes(3)) //circuit breaker will remain in open state for 3 minutes and no calls will be permitted
                 .permittedNumberOfCallsInHalfOpenState(1)  //after 3 minutes in open state, circuit breaker gets half open state. In half open state only 1 call is permitted and if the call fails, circuit breaker opens again. If the call succeeds, circuit breaker gets closed state
                 .build();
