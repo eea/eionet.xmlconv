e1d5886cae57783b6c8f4bd0d438d3aab1b750c6
deniaAthanasakou <antonia.ath@gmail.com>
2022-05-27 16:15:34 +0300

    Created response message for cdr. Refs #150374

--- src/main/java/eionet/gdem/rabbitMQ/service/CdrResponseMessageFactoryServiceImpl.java
+++ src/main/java/eionet/gdem/rabbitMQ/service/CdrResponseMessageFactoryServiceImpl.java
@@ -1,6 +1,7 @@
 package eionet.gdem.rabbitMQ.service;
 
 import eionet.gdem.Constants;
+import eionet.gdem.XMLConvException;
 import eionet.gdem.api.qa.service.QaService;
 import eionet.gdem.jpa.Entities.JobEntry;
 import eionet.gdem.rabbitMQ.model.CdrJobResponseMessage;
@@ -12,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Hashtable;
+import java.util.LinkedHashMap;
 
 @Service("cdrResponseMessageFactoryServiceImpl")
 public class CdrResponseMessageFactoryServiceImpl implements CdrResponseMessageFactoryService{
@@ -27,53 +29,76 @@ public class CdrResponseMessageFactoryServiceImpl implements CdrResponseMessageF
         cdrJobResponseMessage.setUUID(jobEntry.getUuid());
         cdrJobResponseMessage.setJobId(String.valueOf(jobEntry.getId()));
         cdrJobResponseMessage.setJobStatus(StatusUtils.getStatusNameByNumber(jobEntry.getnStatus()));
+        cdrJobResponseMessage.setDocumentURL(jobEntry.getUrl());
+        cdrJobResponseMessage.setScriptId(jobEntry.getQueryId().toString());
+        String scriptFullTitle = jobEntry.getFile();
+        String[] splittedTitleArray = scriptFullTitle.split("/");
+        if(splittedTitleArray.length == 0){
+            cdrJobResponseMessage.setScriptTitle(scriptFullTitle);
+        }
+        else{
+            String scriptTitle = splittedTitleArray[splittedTitleArray.length - 1];
+            cdrJobResponseMessage.setScriptTitle(scriptTitle);
+        }
+
         if(jobEntry.getnStatus() != Constants.XQ_READY && jobEntry.getnStatus() != Constants.XQ_FATAL_ERR && jobEntry.getnStatus() != Constants.XQ_LIGHT_ERR){
             cdrJobResponseMessage.setJobResult(null);
         }
         else{
             //create job result message and set it up
 
+            CdrJobResultMessage jobResult = new CdrJobResultMessage();
+
             Hashtable<String, Object> results = null;
-            /*try {
+            try {
                 results = qaService.getJobResults(String.valueOf(jobEntry.getId()));
 
-                String executionStatusId = (String) results.get(Constants.RESULT_CODE_PRM);
-                String executionStatusName = (String) results.get("executionStatusName");
+                LOGGER.info("For job id " + jobEntry.getId() + " statusId=" + (String) results.get(Constants.RESULT_CODE_PRM) + " and feedbackStatus=" + results.get(Constants.RESULT_FEEDBACKSTATUS_PRM));
+
                 LinkedHashMap<String, Object> jsonResults = new LinkedHashMap<String, Object>();
-                LinkedHashMap<String,String> executionStatusView = new LinkedHashMap<String,String>();
-                executionStatusView.put("statusId", executionStatusId);
-                executionStatusView.put("statusName", executionStatusName);
-                jsonResults.put("scriptTitle",results.get(Constants.RESULT_SCRIPTTITLE_PRM));
-                jsonResults.put("executionStatus",executionStatusView);
-                jsonResults.put("feedbackStatus", results.get(Constants.RESULT_FEEDBACKSTATUS_PRM));
-                jsonResults.put("feedbackMessage", results.get(Constants.RESULT_FEEDBACKMESSAGE_PRM));
-                jsonResults.put("feedbackContentType", results.get(Constants.RESULT_METATYPE_PRM));
-
-                LOGGER.info("For job id " + jobId + " statusId=" + (String) results.get(Constants.RESULT_CODE_PRM) + " and feedbackStatus=" + results.get(Constants.RESULT_FEEDBACKSTATUS_PRM));
-
                 //if result file is zip
                 if(results.get("REMOTE_FILES")!=null){
                     String[] fileUrls = (String[]) results.get("REMOTE_FILES");
                     if(fileUrls[0]!=null) {
-                        jsonResults = qaService.checkIfZipFileExistsOrIsEmpty(fileUrls, jobId, jsonResults);
+                        LinkedHashMap<String,String> executionStatusView = new LinkedHashMap<String,String>();
+                        String executionStatusId = (String) results.get(Constants.RESULT_CODE_PRM);
+                        String executionStatusName = (String) results.get("executionStatusName");
+                        executionStatusView.put("statusId", executionStatusId);
+                        executionStatusView.put("statusName", executionStatusName);
+                        jsonResults.put("executionStatus",executionStatusView);
+                        jsonResults = qaService.checkIfZipFileExistsOrIsEmpty(fileUrls, String.valueOf(jobEntry.getId()), jsonResults);
+                        if(jsonResults.get("REMOTE_FILES") != null)
+                        {
+                            jobResult.setRemoteFiles((String) jsonResults.get("REMOTE_FILES"));
+                        }
+                        if(jsonResults.get("feedbackContent") != null){
+                            jobResult.setFeedbackContent((String) jsonResults.get("feedbackContent"));
+                        }
+                    }
+                    else{
+                        jobResult.setFeedbackContent((String) jsonResults.get(""));
                     }
                 }else{
                     //result file is html
-                    jsonResults = qaService.checkIfHtmlResultIsEmpty(jobId, jsonResults, results);
+                    jsonResults = qaService.checkIfHtmlResultIsEmpty(String.valueOf(jobEntry.getId()), jsonResults, results);
+                    if(jsonResults.get("feedbackContent") != null){
+                        jobResult.setFeedbackContent((String) jsonResults.get("feedbackContent"));
+                    }
                 }
-                if(executionStatusName.equals("Not Found")){
-                    return new ResponseEntity<LinkedHashMap<String, Object>>(jsonResults, HttpStatus.NOT_FOUND);
+                if(results.get(Constants.RESULT_FEEDBACKSTATUS_PRM) != null){
+                    jobResult.setFeedbackStatus((String) results.get(Constants.RESULT_FEEDBACKSTATUS_PRM));
                 }
-                else{
-                    return new ResponseEntity<LinkedHashMap<String, Object>>(jsonResults, HttpStatus.OK);
+                if(results.get(Constants.RESULT_FEEDBACKMESSAGE_PRM) != null){
+                    jobResult.setFeedbackMessage((String) results.get(Constants.RESULT_FEEDBACKMESSAGE_PRM));
                 }
+                if(results.get(Constants.RESULT_METATYPE_PRM) != null){
+                    jobResult.setFeedbackContentType((String) results.get(Constants.RESULT_METATYPE_PRM));
+                }
+                cdrJobResponseMessage.setJobResult(jobResult);
+
             } catch (XMLConvException e) {
                 throw new RuntimeException(e);
-            }*/
-
-
-            CdrJobResultMessage jobResult = new CdrJobResultMessage();
-            cdrJobResponseMessage.setJobResult(jobResult);
+            }
         }
         LOGGER.info("Created response for cdr request and sent it to the queue");
         //send cdrJobResponseMessage to queue
