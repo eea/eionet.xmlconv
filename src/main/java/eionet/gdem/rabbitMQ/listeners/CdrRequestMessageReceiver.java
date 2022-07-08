726ac061c87ccde528eef4007b1789968e4811e8
deniaAthanasakou <antonia.ath@gmail.com>
2022-07-08 15:50:52 +0300

    Sent request that threw exception to DLQ. Refs #150374

--- src/main/java/eionet/gdem/rabbitMQ/listeners/CdrRequestMessageReceiver.java
+++ src/main/java/eionet/gdem/rabbitMQ/listeners/CdrRequestMessageReceiver.java
@@ -10,12 +10,14 @@ import eionet.gdem.rabbitMQ.model.CdrJobRequestMessage;
 import eionet.gdem.rabbitMQ.service.CdrResponseMessageFactoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -36,17 +38,21 @@ public class CdrRequestMessageReceiver implements MessageListener {
     @Override
     public void onMessage(Message message) {
         String messageBody = new String(message.getBody());
+        String uuid = null;
+        List<QaResultsWrapper> qaResults =  new ArrayList<>();
         try {
             ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
             CdrJobRequestMessage cdrMessage = mapper.readValue(messageBody, CdrJobRequestMessage.class);
-            LOGGER.info("Jobs from cdr queue for envelope url " + cdrMessage.getEnvelopeUrl() + " and UUID " + cdrMessage.getUUID() + " will be scheduled");
-            List<QaResultsWrapper> qaResults = qaService.scheduleJobs(cdrMessage.getEnvelopeUrl(), false, true, cdrMessage.getUUID());
-            CdrRequestEntry cdrRequestEntry = new CdrRequestEntry(cdrMessage.getUUID(), cdrMessage.getEnvelopeUrl(), qaResults.size(), new Timestamp(new Date().getTime()));
+            uuid = cdrMessage.getUUID();
+            LOGGER.info("Jobs from cdr queue for envelope url " + cdrMessage.getEnvelopeUrl() + " and UUID " + uuid + " will be scheduled");
+            qaResults = qaService.scheduleJobs(cdrMessage.getEnvelopeUrl(), false, true, uuid);
+            CdrRequestEntry cdrRequestEntry = new CdrRequestEntry(uuid, cdrMessage.getEnvelopeUrl(), qaResults.size(), new Timestamp(new Date().getTime()));
             cdrRequestsService.save(cdrRequestEntry);
-            cdrResponseMessageFactoryService.createCdrSummaryResponseMessageAndSendToQueue(cdrMessage.getUUID(), qaResults);
-            LOGGER.info("There are " + qaResults.size() + " jobs that have been scheduled for envelope url " + cdrMessage.getEnvelopeUrl() + " and UUID " + cdrMessage.getUUID());
+            cdrResponseMessageFactoryService.createCdrSummaryResponseMessageAndSendToQueue(uuid, qaResults);
+            LOGGER.info("There are " + qaResults.size() + " jobs that have been scheduled for envelope url " + cdrMessage.getEnvelopeUrl() + " and UUID " + uuid);
         } catch (Exception e) {
             LOGGER.error("Error during cdr message processing. Exception message is:  " + e.getMessage());
+            throw new AmqpRejectAndDontRequeueException(e.getMessage());
         }
     }
 }
