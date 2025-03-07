648d79f4d89f09d25e58a918a5eea96804620033
js <js@eworx.gr>
2025-03-07 11:04:46 +0200

    Refs #285137 integration tests with mysql 8

--- src/test/java/eionet/gdem/test/ApplicationTestContext.java
+++ src/test/java/eionet/gdem/test/ApplicationTestContext.java
@@ -26,7 +26,7 @@ import org.springframework.context.annotation.ImportResource;
  */
 //@Configuration
 @ImportResource({"classpath:test-spring-app-context.xml", "classpath:test-runtime.xml",
-        "classpath:spring-datasource-context.xml"})
+        "classpath:test-spring-datasource.xml"})
 public class ApplicationTestContext {
 
 }
