3ab06d618549fb4aa4bcef59f2481b47a8cf4248
js <js@eworx.gr>
2025-06-04 16:58:01 +0300

    Refs #287716 plain junit test

--- src/test/java/eionet/gdem/CopyCatalogFileActionIT.java
+++ src/test/java/eionet/gdem/CopyCatalogFileActionIT.java
@@ -1,22 +1,16 @@
 package eionet.gdem;
 
 import eionet.gdem.configuration.CopyCatalogFileAction;
-import eionet.gdem.test.ApplicationTestContext;
 import org.apache.commons.io.FileUtils;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 
 /**
  * @author George Sofianos
  */
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { ApplicationTestContext.class })
 public class CopyCatalogFileActionIT {
 
     @Rule
